5c07c1129c9ca66eb3cc9ec9ba889ea12331815c
24LP6048 24LP6048 <24LP6048@G302-CC0089.local>
2024-12-25 17:27:27 +0800

    added components

---
+++ src/components/layout.tsx
@@ -0,0 +1,25 @@
+// import type {MetaData} from'next';
+// import{Geist,Geist_Mono} from 'next/font/google';
+// import'./globals.css';
+// import{Footer} from './_components/Footer';
+// import { CardDescription } from './ui/card';
+
+// const geistSans=Geist({
+//     variable:'--font--geist--sans',
+//     subsets:['latin'],
+// });
+// const  geistMono=Geist_Mono({
+//     variable:'--font--geist--mono',
+//     subsets:['latin'],
+// });
+// export const metdata:MetaData={
+//     title:'Movie App',
+//     description:'Looktv',
+// };
+// export default function RootLayout({
+//     children,
+// }Readonly<{
+//     children: React.ReactNode;
+// }>) 
+
+    
